246f0e7118b03daea975ee71d8fbb29555ab3946
JohnVindel <jev30_vindel@hotmail.com>
2021-06-16 22:44:52 -0600

    primer commit

---
+++ Semana2-Tarea1-EncontrarAreaTriangulo.py
@@ -0,0 +1,10 @@
+#// Calcular el area de un triangulo//#
+
+#Declaramos las variables
+Altura = int(input("Ingrese la altura: \n"))
+Base = int(input("Ingrese la base: \n"))
+
+Area = Base * Altura / 2
+
+print("Y su area es: ", Area)
+
